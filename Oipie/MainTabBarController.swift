5a4d9f2f1b14102484c6ec8de77de347d002e666
Daniel Ramos <danielramosacosta1@gmail.com>
2022-08-29 10:59:23 +0100

    working TabBar layout

--- Oipie/MainTabBarController.swift
+++ Oipie/MainTabBarController.swift
@@ -9,34 +9,41 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     let homeViewController: HomeViewController
-    let upcomingViewController: HomeViewController
-    let searchViewController: HomeViewController
-    let downloadsViewController: HomeViewController
+    let searchViewController: SearchViewController
+    let favouritesViewController: FavouritesViewController
+    let doneRecepiesViewController: DoneRecepiesViewController
+    let profileViewController: ProfileViewController
+    
+    
 
     static func build() -> MainTabBarController {
         let homeViewController = HomeViewController()
-        let upcomingViewController = HomeViewController()
-        let searchViewController = HomeViewController()
-        let downloadsViewController = HomeViewController()
+        let searchViewController = SearchViewController()
+        let favouritesViewController = FavouritesViewController()
+        let doneRecepiesViewController = DoneRecepiesViewController()
+        let profileViewController = ProfileViewController()
 
         return MainTabBarController(
             homeViewController: homeViewController,
-            upcomingViewController: upcomingViewController,
             searchViewController: searchViewController,
-            downloadsViewController: downloadsViewController
+            favouritesViewController: favouritesViewController,
+            doneRecepiesViewController: doneRecepiesViewController,
+            profileViewController: profileViewController
         )
     }
 
     init(
         homeViewController: HomeViewController,
-        upcomingViewController: HomeViewController,
-        searchViewController: HomeViewController,
-        downloadsViewController: HomeViewController
+        searchViewController: SearchViewController,
+        favouritesViewController: FavouritesViewController,
+        doneRecepiesViewController: DoneRecepiesViewController,
+        profileViewController: ProfileViewController
     ) {
         self.homeViewController = homeViewController
-        self.upcomingViewController = upcomingViewController
         self.searchViewController = searchViewController
-        self.downloadsViewController = downloadsViewController
+        self.favouritesViewController = favouritesViewController
+        self.doneRecepiesViewController = doneRecepiesViewController
+        self.profileViewController = profileViewController
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -46,25 +53,18 @@ class MainTabBarController: UITabBarController {
     func loadTabBar() {
         let tabItems: [TabItem] = [.home, .search, .favourites, .doneRecepies, .profile]
 
-        setupCustomTabMenu(tabItems) { controllers in
-            self.viewControllers = controllers
-        }
-
-        selectedIndex = 0 // default our selected index to the first item
+        setupCustomTabMenu(tabItems)
     }
 
-    func setupCustomTabMenu(_ items: [TabItem], completion: @escaping ([UIViewController]) -> Void) {
+    func setupCustomTabMenu(_ items: [TabItem]) {
         let frame = tabBar.frame
-        var controllers = [UIViewController]()
-        // hide the tab bar
+
         tabBar.isHidden = true
         customTabBar = TabNavigationMenu(items, frame: frame)
         customTabBar.translatesAutoresizingMaskIntoConstraints = false
         customTabBar.clipsToBounds = true
         customTabBar.itemTapped = changeTab
-        // Add it to the view
         view.addSubview(customTabBar)
-        // Add positioning constraints to place the nav menu right where the tab bar should be
         NSLayoutConstraint.activate([
             customTabBar.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor),
             customTabBar.trailingAnchor.constraint(equalTo: tabBar.trailingAnchor),
@@ -72,11 +72,7 @@ class MainTabBarController: UITabBarController {
             customTabBar.heightAnchor.constraint(equalToConstant: tabBarHeight), // Fixed height for nav menu
             customTabBar.bottomAnchor.constraint(equalTo: tabBar.bottomAnchor),
         ])
-        for i in 0 ..< items.count {
-            controllers.append(items[i].viewController) // we fetch the matching view controller and append here
-        }
         view.layoutIfNeeded() // important step
-        completion(controllers) // setup complete. handoff here
     }
 
     func changeTab(tab: Int) {
@@ -92,25 +88,90 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
 
         let vc1 = UINavigationController(rootViewController: homeViewController)
-        let vc2 = UINavigationController(rootViewController: upcomingViewController)
-        let vc3 = UINavigationController(rootViewController: searchViewController)
-        let vc4 = UINavigationController(rootViewController: downloadsViewController)
+        let vc2 = UINavigationController(rootViewController: searchViewController)
+        let vc3 = UINavigationController(rootViewController: favouritesViewController)
+        let vc4 = UINavigationController(rootViewController: doneRecepiesViewController)
+        let vc5 = UINavigationController(rootViewController: profileViewController)
 
-        vc1.tabBarItem.image = UIImage(systemName: "house")
-        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
-        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
-        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        vc1.tabBarItem.image = UIImage(named: "home")
+        vc2.tabBarItem.image = UIImage(named: "search")
+        vc3.tabBarItem.image = UIImage(named: "heart")
+        vc4.tabBarItem.image = UIImage(named: "page-flip")
+        vc5.tabBarItem.image = UIImage(named: "profile-circled")
 
+        
+        
         vc1.title = "Home"
-        vc2.title = "Coming Soon"
-        vc3.title = "Search"
-        vc4.title = "Downloads"
+        vc2.title = "Search"
+        vc3.title = "Favourites"
+        vc4.title = "Done Recepies"
+        vc5.title = "Profile"
 
         tabBar.backgroundColor = .white
         tabBar.tintColor = .black
 
-        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        setViewControllers2([vc1, vc2, vc3, vc4, vc5], animated: true)
 
-        loadTabBar()
+        // loadTabBar()
+    }
+    
+    func setViewControllers2(_ controllers: [UINavigationController], animated: Bool) {
+        tabBar.isHidden = true
+        
+        let customTabBar = UIView(frame: tabBar.frame)
+        customTabBar.backgroundColor = .white
+        customTabBar.translatesAutoresizingMaskIntoConstraints = false
+        customTabBar.clipsToBounds = true
+        customTabBar.layer.cornerRadius = 45.0
+        customTabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        view.addSubview(customTabBar)
+        
+        NSLayoutConstraint.activate([
+            customTabBar.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor),
+            customTabBar.trailingAnchor.constraint(equalTo: tabBar.trailingAnchor),
+            customTabBar.widthAnchor.constraint(equalToConstant: tabBar.frame.width),
+            customTabBar.heightAnchor.constraint(equalToConstant: 116.0), // Fixed height for nav menu
+            customTabBar.bottomAnchor.constraint(equalTo: tabBar.bottomAnchor),
+        ])
+        
+        viewControllers = controllers
+        
+        let colors: [UIColor] = [.systemBlue, .systemRed, .systemPink, .systemGreen, .systemBrown]
+        for i in 0 ..< controllers.count {
+            let customItem = UIView(frame: .zero)
+            customItem.backgroundColor = colors[i]
+            customItem.translatesAutoresizingMaskIntoConstraints = false
+            customItem.clipsToBounds = true
+            
+            let itemWidth = customTabBar.frame.width / CGFloat(5)
+            let leadingAnchor = itemWidth * CGFloat(i)
+            
+            customTabBar.addSubview(customItem)
+            
+            NSLayoutConstraint.activate([
+                customItem.heightAnchor.constraint(equalTo: customTabBar.heightAnchor),
+                customItem.leadingAnchor.constraint(equalTo: customTabBar.leadingAnchor, constant: leadingAnchor),
+                customItem.widthAnchor.constraint(equalToConstant: itemWidth),
+                customItem.topAnchor.constraint(equalTo: customTabBar.topAnchor),
+            ])
+        }
+        
+        /*
+        let customTabBar = UIView()
+        let height = 200.0
+        customTabBar.frame = CGRect(x: 0, y: view.bounds.height - height, width: view.bounds.width, height: height)
+
+        let rectShape = CAShapeLayer()
+        rectShape.bounds = customTabBar.frame
+        rectShape.position = customTabBar.center
+        rectShape.path = UIBezierPath(roundedRect: customTabBar.bounds, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: 45, height: 45)).cgPath
+
+        customTabBar.layer.backgroundColor = UIColor.green.cgColor
+        customTabBar.layer.mask = rectShape
+        customTabBar.backgroundColor = .white
+        
+        view.addSubview(customTabBar)
+        viewControllers = controllers*/
     }
 }
